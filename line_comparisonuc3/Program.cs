5541c7257af20046b8cb7cb10ecc764ef7686d0a
Tulasi ram <18btrec047@jainuniversity.ac.in.com>
2022-03-04 17:29:38 +0530

    added day-8 practice problems

---
+++ line_comparisonuc3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace line_comparisonuc3
+{
+    internal class Program
+    {
+        //variables
+        double length_Of_Line;
+
+        //method for calculation
+        public void Check(double x1, double y1, double x2, double y2)
+        {
+            double X = Math.Pow(x2 - x1, 2.0);
+            double Y = Math.Pow(y2 - y1, 2.0);
+            length_Of_Line = Math.Sqrt(X + Y);
+        }
+        static void Main(string[] args)
+        {
+            // creating object 1 and reading input from user
+            Program p1 = new Program();
+            Console.WriteLine("enter line 1 values x1,y1,x2,y2 values\n");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());
+            double c = Convert.ToDouble(Console.ReadLine());
+            double d = Convert.ToDouble(Console.ReadLine());
+            p1.Check(a, b, c, d);
+            Console.WriteLine("Line1 Length is = " + p1.length_Of_Line);
+
+            // creating object 2 and reading input from user
+            Program p2 = new Program();
+            Console.WriteLine("Enter Sencond line values x1,y1,x2,y2\n");
+            double e = Convert.ToDouble(Console.ReadLine());
+            double f = Convert.ToDouble(Console.ReadLine());
+            double g = Convert.ToDouble(Console.ReadLine());
+            double h = Convert.ToDouble(Console.ReadLine());
+            p2.Check(e, f, g, h);
+            Console.WriteLine("Line 2 Length is = " + p2.length_Of_Line);
+            //comparing both line values to if its equal Returns True if equal else False
+            Console.WriteLine("Checking lines are equal the answer is");
+            Console.WriteLine((p1.length_Of_Line).Equals(p2.length_Of_Line));
+            //comparing both line values to find line 1 is greater than line 2 Returns "1" if line 1 is greater then line, if line 1 is less than line 2 "-1"
+            Console.WriteLine("Compairing line 1 to line 2 \nif returned 1 :line 1 is greater than line 2\nif returned -1 :line 1 is less than line 2");
+            Console.WriteLine((p1.length_Of_Line).CompareTo(p2.length_Of_Line));
+        }
+
+    }
+}
+        
+    
+
---
+++ line_equationuc2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace line_equationuc2
+{
+    internal class Program
+    {
+        public static void Check(int x1, int x2, int y1, int y2, int p1, int p2, int q1, int q2)
+        {
+            int a1 = (y2 - y1) * p1 + (x1 - x2) * q1 + (x2 * y1 - x1 * y2);
+
+            if (a1 < 0)
+            {
+                int a2 = (y2 - y1) * p2 + (x1 - x2) * q2
+                     + (x2 * y1 - x1 * y2);
+                if (a2 >= 0)
+                    Console.WriteLine("Intersecting");
+                else if (a2 < 0)
+                    Console.WriteLine("Not intersecting");
+            }
+            else if (a1 > 0)
+            {
+
+                int a2 = (y2 - y1) * p2 + (x1 - x2) * q2
+                         + (x2 * y1 - x1 * y2);
+
+                if (a2 <= 0)
+                    Console.WriteLine("Intersecting");
+
+
+                else if (a2 > 0)
+                    Console.WriteLine("Not intersecting");
+            }
+            else
+                Console.WriteLine(
+                    "points are lying on the line");
+        }
+            static void Main(string[] args)
+        {
+            int x1 = 2, y1 = 3;
+            int x2 = 6, y2 = 4;
+
+            Console.WriteLine("Equation 1: (" + (y2 - y1)
+                          + ")x+(" + (x1 - x2) + ")y+("
+                          + (x2 * y1 - x1 * y2) + ") = 0");
+
+            int p1 = 3, q1 = 4;
+            int p2 = 7, q2 = 1;
+
+            Console.WriteLine("Equation 2: (" + (q2 - q1)
+                           + ")x+(" + (p1 - p2) + ")y+("
+                           + (p2 * q1 - p1 * q2) + ") = 0");
+
+            Check(x1, x2, y1, y2, p1, p2, q1, q2);
+        }
+    }
+}
